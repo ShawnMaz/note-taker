7d4642cdf95d3d2fa1e5245e278fc2d8819ffe56
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-09-14 23:48:20 -0400

    added the function to check if a note exists in the database or not

--- __test__/notes.test.js
+++ __test__/notes.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const {createNewNotes, readNotes, deleteNotes, validateNote} = require('../lib/notes');
+const {createNewNotes, readNotes, deleteNotes, validateNote, isFound} = require('../lib/notes');
 
 jest.mock('fs');
 
@@ -86,4 +86,37 @@ test('validates note', () => {
     expect(result1).toEqual(false);
     expect(result2).toEqual(false);
     expect(result3).toEqual(true);
+});
+
+test('checks if an ID exists in the database', () => {
+    const noteExists = {
+        id : '1',
+        title: 'Note title',
+        text: 'Note text'
+    }
+
+    const noteNotExist = {
+        id : '5',
+        title: 'Note title',
+        text: 'Note text'
+    }
+
+    const notesArray = [
+        {
+            id:'1',
+            title:'Note title 1',
+            text:'Note text 1'
+        },
+        {
+            id: '2',
+            title: 'Note title 2',
+            text: 'Note text 2'
+        }
+    ]
+
+    const resultExist = isFound(noteExists, notesArray);
+    const resultNotExist = isFound(noteNotExist, notesArray);
+
+    expect(resultExist).toEqual(true);
+    expect(resultNotExist).toEqual(false);
 });
